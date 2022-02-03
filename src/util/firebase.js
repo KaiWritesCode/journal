68d903a2b66006d786a469cd130c768abe64a4b7
Kai Bechdel <kbechdel8@gmail.com>
2022-02-03 12:09:34 -0500

    Initial commit

---
+++ src/util/firebase.js
@@ -0,0 +1,15 @@
+import { initializeApp } from 'firebase/app'
+
+const firebaseConfig = {
+    apiKey: "AIzaSyBz7NoASkJjWvBzuwDyur1e0qIu4qb4Fqw",
+    authDomain: "journal-anything.firebaseapp.com",
+    projectId: "journal-anything",
+    storageBucket: "journal-anything.appspot.com",
+    messagingSenderId: "300217607547",
+    appId: "1:300217607547:web:026a9168a4a9cd611d89de"
+};
+
+// Initialize Firebase
+
+const app = initializeApp(firebaseConfig)
+
